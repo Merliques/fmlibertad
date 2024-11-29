398ca2fc9ec73b3df1b693ab9fe602d73ede3c2f
infortronix <norbertovaldeon@gmail.com>
2024-11-29 14:02:03 -0300

    index css y js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const carouselItems = document.querySelectorAll('.carousel-item');
+let currentIndex = 0;
+
+function showNextSlide() {
+  carouselItems[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  carouselItems[currentIndex].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000);
